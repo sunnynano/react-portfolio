667c8a522b403c858791b0ef3c097815a6e85992
Sunny <sunnyguan2018@gmail.com>
2024-07-21 16:22:18 -0700

    Add project

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,9 @@
-import Contact from "../pages/Contact"
 import About from "../pages/About"
 import Projects from "../pages/Projects"
 import SingleProject from "../pages/SingleProject"
 import Home from "../pages/Home"
 
 const routes = [
-  {
-    path: "/contact",
-    element: <Contact />,
-  },
   {
     path: "/about",
     element: <About/>,
